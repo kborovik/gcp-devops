2e392df9f3b91655c135329a2c20d67ac4d2b7c5
Konstantin Borovik <github@lab5.ca>
2026-02-16 10:09:06 -0500

    add backup documentation and reorganize snapshot policy
    - Add backup architecture overview and recovery procedures to README
    - Move GCE disk snapshot policy from gce-mailpilot.tf to gcs-backups.tf
      to co-locate all backup resources
    - Enable prevent_destroy on data disk for safety

--- terraform/gce-mailpilot.tf
+++ terraform/gce-mailpilot.tf
@@ -54,7 +54,7 @@ resource "google_compute_disk" "mailpilot_data" {
   depends_on = [google_project_service.main]
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
 
@@ -130,37 +130,6 @@ resource "google_compute_instance" "mailpilot" {
       boot_disk[0].initialize_params[0].image
     ]
   }
-}
-
-###############################################################################
-# Data Disk Snapshot Policy
-###############################################################################
-
-resource "google_compute_resource_policy" "data_disk_snapshots" {
-  name   = "data-disk-daily-snapshots"
-  region = var.google_region
-
-  snapshot_schedule_policy {
-    schedule {
-      daily_schedule {
-        days_in_cycle = 1
-        start_time    = "02:00"
-      }
-    }
-
-    retention_policy {
-      max_retention_days    = 14
-      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
-    }
-  }
-
-  depends_on = [google_project_service.main]
-}
-
-resource "google_compute_disk_resource_policy_attachment" "data_disk_snapshots" {
-  name = google_compute_resource_policy.data_disk_snapshots.name
-  disk = google_compute_disk.mailpilot_data.name
-  zone = "${var.google_region}-b"
 }
 
 ###############################################################################
--- terraform/gcs-backups.tf
+++ terraform/gcs-backups.tf
@@ -26,3 +26,34 @@ resource "google_storage_bucket_iam_member" "backups_writer" {
   role   = "roles/storage.objectAdmin"
   member = google_service_account.mailpilot.member
 }
+
+###############################################################################
+# Data Disk Snapshot Policy
+###############################################################################
+
+resource "google_compute_resource_policy" "data_disk_snapshots" {
+  name   = "data-disk-daily-snapshots"
+  region = var.google_region
+
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "02:00"
+      }
+    }
+
+    retention_policy {
+      max_retention_days    = 14
+      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+    }
+  }
+
+  depends_on = [google_project_service.main]
+}
+
+resource "google_compute_disk_resource_policy_attachment" "data_disk_snapshots" {
+  name = google_compute_resource_policy.data_disk_snapshots.name
+  disk = google_compute_disk.mailpilot_data.name
+  zone = "${var.google_region}-b"
+}
