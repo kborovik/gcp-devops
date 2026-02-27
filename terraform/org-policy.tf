a9ce514ef6217813a86c38c5a15e7e6f79a5e7a2
Konstantin Borovik <github@lab5.ca>
2026-02-27 11:02:41 -0500

    chore(terraform): override allowedPolicyMemberDomains org policy for Gmail push
    The org constraint iam.allowedPolicyMemberDomains blocks Gmail's
    gmail-api-push@system.gserviceaccount.com from Pub/Sub topic IAM,
    preventing push notifications. Add a toggleable org policy override
    (enabled for dev, off by default for prod).

--- terraform/org-policy.tf
+++ terraform/org-policy.tf
@@ -2,6 +2,20 @@
 # Organization Policy Overrides
 ###############################################################################
 
+resource "google_org_policy_policy" "allow_external_iam_members" {
+  count  = var.allow_external_iam_members ? 1 : 0
+  name   = "projects/${var.google_project}/policies/iam.allowedPolicyMemberDomains"
+  parent = "projects/${var.google_project}"
+
+  spec {
+    rules {
+      allow_all = "TRUE"
+    }
+  }
+
+  depends_on = [google_project_service.main]
+}
+
 resource "google_org_policy_policy" "allow_sa_key_creation" {
   count  = var.allow_sa_key_creation ? 1 : 0
   name   = "projects/${var.google_project}/policies/iam.disableServiceAccountKeyCreation"
